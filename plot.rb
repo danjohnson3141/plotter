443cfed52e48c9d59c2c9ce6a3644a26e1eb72f5
Dan Johnson <daniel.johnson@evanta.com>
2015-10-10 11:10:28 -0700

    initial commit

---
+++ plot.rb
@@ -0,0 +1,70 @@
+# TODO: Improve class to allow for plotting of more then just upper-left to lower-right
+
+class Plot
+  attr_reader :file_contents, :x1_y1, :x2_y2
+
+  def initialize(filename)
+    @file_contents = File.read(filename).split("\n")
+    @x1_y1 = find_point(1)
+    @x2_y2 = find_point(2)
+  end
+
+  def find_x(element)
+    element.rindex('#')
+  end
+
+  def find_point(num)
+    cnt = 0
+    file_contents.each_with_index do |file_content, row|
+      cnt += 1 if find_x(file_content)
+      return [find_x(file_content), row] if cnt == num
+    end
+  end
+
+  def show_info
+    print "Coordinates:\n#{sep}\n"
+    puts "x1: #{x1_y1[0].to_s.rjust(3)}, y1: #{x1_y1[1].to_s.rjust(3)}"
+    puts "x2: #{x2_y2[0].to_s.rjust(3)}, y2: #{x2_y2[1].to_s.rjust(3)}"
+    puts "#{sep}\nOutput:\n#{sep}\n"
+    puts direction + "\n"
+  end
+
+  def draw(info)
+    show_info if info
+    started = false
+      lines = []
+      (x2_y2[1] + 1).times do |i|
+      if x1_y1[1] == i || started
+        lines[i] = (' ' * x1_y1[0]) + '#'
+        started = true
+      end
+      if x2_y2[1] == i
+        if direction == 'right'
+          lines[i] += ('#' * (x2_y2[0] - (x1_y1[0] - 1)))
+        else
+          lines[i] = (' ' * x2_y2[0]) + ('#' * (x1_y1[0] - (x2_y2[0] - 1)))
+        end
+      end
+    end
+    lines.each { |line| puts line }
+  end
+
+  def direction
+    return 'right' if x1_y1[0] < x2_y2[0]
+    'left'
+  end
+
+  def check_file?
+    show_info
+    x1_y1[0] < x2_y2[0]
+  end
+
+  def sep(num = 40)
+    '-' * 40
+  end
+
+  def output(info = false)
+    # return unless check_file?
+    draw(info)
+  end
+end
